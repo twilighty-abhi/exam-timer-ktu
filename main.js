0258c434fb756088fc486f5b3d197ecf3fb3ce08
Abhiram N J <abhiramnj@gmail.com>
2024-05-17 13:24:32 +0530

    Feat: Include warning colour for time remaining🌚

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ function countdown(cdate, dest) {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        if (days < 5) {
+            dest.style.color = "red";
+        } else {
+            dest.style.color = "black";
+        }
+        
         dest.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     }, 1000);
 }
